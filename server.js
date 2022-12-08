ad71455f1f0d9c833e19959169c5bf7d72739c54
Imhotep-education <119817315+Imhotep-education@users.noreply.github.com>
2022-12-08 06:53:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -78,6 +78,6 @@ app.post('/register', (req, res)=>{
 	})
 })
 const server = http.createServer(app);
-server.listen(3000, ()=>{
+server.listen(process.env.PORT, ()=>{
 	console.log('Server is running on port 3000')
 })
