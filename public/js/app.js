7ee891e1f96e8135397385029962b42ab72eddee
Imhotep-education <imhotep.oficial@gmail.com>
2022-12-08 06:03:11 -0300

    archivos subidos

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+var scene = new xeogl.Scene();
+
+var material = new xeogl.PhongMaterial(scene, {
+    diffuse: [ 0.6, 0.6, 0.7 ]
+});
+
+var geometry = new xeogl.TorusGeometry(scene);
+
+var mesh = new xeogl.Mesh(scene, {
+    material: material,
+    geometry: geometry
+});
+
+material.diffuse = [1.0, 1.0, 0.0];
+material.alpha = 0.5;
+
+scene.on("tick", function () {
+    scene.camera.orbitYaw(0.6);
+    scene.camera.orbitPitch(0.3);
+});
--- server.js
+++ server.js
@@ -1,2 +1,83 @@
-const express = require("express");
+const express = require('express')
 const app = express()
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const http = require('http')
+const socketIo = require('socket.io')
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res)=>{
+	res.render('index')
+})
+
+app.get('/account', (req, res)=>{
+	res.render('account', {
+		user: 'Igna2005',
+		crono: {
+			primera: {
+				materia: 'Lengua',
+				nota: undefined,
+				hora: '12:30'
+			},
+			segunda: {
+				materia: 'Circuitos electronicos I',
+				nota: 'Prueba',
+				hora: '13:10'
+			}, 
+			tercera: {
+				materia: 'Circuitos electronicos I',
+				nota: 'Prueba',
+				hora: '13:50'
+			},
+			cuarta: {
+				materia: 'Circuitos electronicos I',
+				nota: 'TP f.d.p',
+				hora: '14:30',
+			},
+			quinta: {
+				materia: 'Circuitos electronicos I',
+				nota: 'TP f.d.p',
+				hora: '15:10',
+			},
+			sexta: {
+				materia: 'Matematica',
+				nota: 'Prueba',
+				hora: '15:50',
+			},
+			septima: {
+				materia: 'Matematica',
+				nota: 'Prueba',
+				hora: '16:30'
+			}
+		},
+		tareas: {
+			lengua: {
+				
+			}
+		}
+	})
+})
+
+app.get('/escritorio', (req, res)=>{
+	res.render('escritorio')
+})
+
+app.post('/register', (req, res)=>{
+	const email    = req.body.email
+	const password = req.body.password
+	const role     = req.body.role
+
+	res.render('/account', {
+		email: email,
+		role: role,
+		action: 'register-form'
+	})
+})
+const server = http.createServer(app);
+server.listen(8080, ()=>{
+	console.log('Server is running on port 8080')
+})
